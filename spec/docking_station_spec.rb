b597a74bc94f4b0d79889934e9b5ba04e193c5d3
Elizabeth Wicks <elizabeth.wicks@hotmail.com>
2016-09-28 17:32:47 +0100

    Arguments

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,10 +3,10 @@ require 'docking_station'
 describe DockingStation do
   let(:docking_station) {DockingStation.new}
   let(:bike) {Bike.new}
-  it {expect(docking_station).to respond_to (:release_bike}
+  it {expect(docking_station).to respond_to (:release_bike)}
   it {expect(docking_station.release_bike).to be_instance_of(Bike)}
   it {expect(bike.working?).to be true}
-  it {expect(docking_station).to respond_to (:dock_bike)}
+  it {expect(docking_station).to respond_to(:dock_bike).with(1).argument}
   it {expect(docking_station.dock_bike).to be_instance_of (Bike)}
   it {expect(docking_station).to respond_to (:bike)}
 
