0315753f8a273ad71da3aed9ad7a816d5025810f
Royston <rlqhenson@hotmail.com>
2016-09-29 13:51:52 +0100

    testing makersinit

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,17 +1,24 @@
+#testing makersinit
 require_relative "bike"
 
 class DockingStation
-  attr_reader :bike
+  #attr_reader :bike
 
+  def initialize(bike = [])
+    @bike = bike
+  end
 
   def release_bike
-    fail "no bikes available" unless @bike
-    @bike
+    fail "no bikes available" unless @bike.not_empty?
+    @bike.pop
+
   end
 
   def dock_bike(bike)
-    fail "Sorry! The dock is full!" if @bike
-    @bike = bike
+    fail "Sorry! The dock is full!" if @bike.length >= 20
+    @bike << bike
+
+
   end
 
 end
