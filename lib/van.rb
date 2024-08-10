38b812d07ffb95ac420cc56a9e6f4bde6d477ce9
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-08-11 00:59:56 +0100

    Enable Van to pickup all broken bikes for transport

--- lib/van.rb
+++ lib/van.rb
@@ -1,7 +1,17 @@
 class Van
   attr_reader :storage
-  
+
   def initialize
     @storage = []
+  end
+
+  def load_bikes(array)
+    array.map { |bike|
+                if bike.working == false
+                  storage << bike
+                  array.delete_at(array.index(bike))
+                end
+    }
+    #storage << array.delete_at(array.index())
   end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -8,4 +8,17 @@ describe Van do
       expect(van.storage).to eq([])
     end
   end
+
+  describe '#load_bikes' do
+    context 'when picking up broken bikes' do
+      it 'loads only broken bikes into it\'s storage' do
+        bike1, bike3 = double('bike1', :working => false), double('bike3', :working => false)
+        bike2 = double('bike2', :working => true)
+        bike_rack = [bike1, bike2, bike3]
+        van.load_bikes(bike_rack)
+        expect(van.storage).to eq([bike1, bike3])
+        expect(bike_rack).to eq([bike2])
+      end
+    end
+  end
 end
