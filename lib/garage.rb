d99c38fc3e3b96dba885366eb966251214f3029f
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-08-12 18:17:40 +0100

    Add storage to Garage so they can accept deliveries

--- lib/garage.rb
+++ lib/garage.rb
@@ -1,3 +1,7 @@
 class Garage
-
+  attr_reader :storage
+  
+  def initialize
+    @storage = []
+  end
 end
--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -1,4 +1,10 @@
 require 'garage'
 
 describe Garage do
+  let(:garage) { Garage.new }
+  describe '#initialize' do
+    it 'has a storage area loading/unloading deliveries' do
+      expect(garage.storage).to eq([])
+    end
+  end
 end
