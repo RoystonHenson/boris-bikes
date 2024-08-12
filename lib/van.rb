72aa4cb1bf1aed483bf397eded84f151a230249f
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-08-12 15:19:07 +0100

    Enable van to drop off broken bikes at a garage for repairs, fix #load_bikes to correctly move all broken bikes

--- lib/van.rb
+++ lib/van.rb
@@ -1,17 +1,25 @@
+require './lib/docking_station'
+
 class Van
-  attr_reader :storage
+  attr_accessor :storage
 
   def initialize
     @storage = []
   end
 
   def load_bikes(array)
-    array.map { |bike|
-                if bike.working == false
-                  storage << bike
-                  array.delete_at(array.index(bike))
-                end
-    }
-    #storage << array.delete_at(array.index())
+    # move bikes to the van storage first
+    array.select { |bike| 
+                   storage << bike if bike.working == false
+                 }
+    # after all bikes have been moved, set the array to only hold the values that should still be there ('select!'?)
+    array.select! { |bike| bike.working == true }
+  end
+
+  def unload_bikes(array)
+    storage.select { |bike| 
+                    array << bike if bike.working == false
+                   }
+    storage.select! { |bike| bike.working == true }
   end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -2,6 +2,10 @@ require 'van'
 
 describe Van do
   let(:van) { Van.new }
+  let(:bike1) { double('bike1', :working => false) }
+  let(:bike2) { double('bike2', :working => true ) }
+  let(:bike3) { double('bike3', :working => false) }
+  let(:bike4) { double('bike4', :working => false) }
 
   describe '#initialize' do
     it 'has storage to load items into for transport' do
@@ -12,12 +16,22 @@ describe Van do
   describe '#load_bikes' do
     context 'when picking up broken bikes' do
       it 'loads only broken bikes into it\'s storage' do
-        bike1, bike3 = double('bike1', :working => false), double('bike3', :working => false)
-        bike2 = double('bike2', :working => true)
         bike_rack = [bike1, bike2, bike3]
         van.load_bikes(bike_rack)
         expect(van.storage).to eq([bike1, bike3])
         expect(bike_rack).to eq([bike2])
+      end
+    end
+  end
+
+  describe 'unload_bikes' do
+    context 'when delivering broken bikes' do
+      it 'offloads only broken bikes from its storage' do
+        garage = double('garage', :storage => [])
+        van.storage = [bike1, bike2, bike3, bike4]
+        van.unload_bikes(garage.storage)
+        expect(van.storage).to eq([bike2])
+        expect(garage.storage).to eq([bike1, bike3, bike4])
       end
     end
   end
